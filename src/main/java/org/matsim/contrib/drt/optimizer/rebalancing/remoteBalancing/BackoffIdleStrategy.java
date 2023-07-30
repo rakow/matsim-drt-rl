1c623a3ce798807236bf62a6a6d0305c0a324014
rakow <rakow@vsp.tu-berlin.de>
2023-07-30 13:17:20 +0200

    update grpc, simplified backoff idle

--- src/main/java/org/matsim/contrib/drt/optimizer/rebalancing/remoteBalancing/BackoffIdleStrategy.java
+++ src/main/java/org/matsim/contrib/drt/optimizer/rebalancing/remoteBalancing/BackoffIdleStrategy.java
@@ -3,14 +3,10 @@ package org.matsim.contrib.drt.optimizer.rebalancing.remoteBalancing;
 import java.util.concurrent.locks.LockSupport;
 
 /**
- * Adapted from <a href="https://github.com/real-logic/agrona/blob/master/agrona/src/main/java/org/agrona/concurrent/BackoffIdleStrategy.java">...</a>
+ * Parks the threads with increasing number of nanoseconds.
  */
 final class BackoffIdleStrategy {
 
-	/**
-	 * Default number of times the strategy will yield without work before going to next state.
-	 */
-	public static final long DEFAULT_MAX_YIELDS = 15L;
 	/**
 	 * Default minimum interval the strategy will park a thread.
 	 */
@@ -19,27 +15,6 @@ final class BackoffIdleStrategy {
 	 * Default maximum interval the strategy will park a thread.
 	 */
 	public static final long DEFAULT_MAX_PARK_PERIOD_NS = 1_000_000L;
-	/**
-	 * Denotes a non-idle state.
-	 */
-	private static final int NOT_IDLE = 0;
-	/**
-	 * Denotes a yielding state.
-	 */
-	private static final int YIELDING = 2;
-	/**
-	 * Denotes a parking state.
-	 */
-	private static final int PARKING = 3;
-	/**
-	 * Current state.
-	 */
-	private int state = NOT_IDLE;
-
-	/**
-	 * Number of yields.
-	 */
-	private long yields;
 
 	/**
 	 * Park period in nanoseconds.
@@ -51,35 +26,15 @@ final class BackoffIdleStrategy {
 	 * Perform idle operation.
 	 */
 	void idle() {
-		switch (state) {
-			case NOT_IDLE:
-				state = YIELDING;
-				yields++;
-				break;
-
-			case YIELDING:
-				if (++yields > DEFAULT_MAX_YIELDS) {
-					state = PARKING;
-					parkPeriodNs = DEFAULT_MIN_PARK_PERIOD_NS;
-				} else {
-					Thread.yield();
-				}
-				break;
-
-			case PARKING:
-				LockSupport.parkNanos(parkPeriodNs);
-				parkPeriodNs = Math.min(parkPeriodNs << 1, DEFAULT_MAX_PARK_PERIOD_NS);
-				break;
-		}
+		LockSupport.parkNanos(parkPeriodNs);
+		parkPeriodNs = Math.min(parkPeriodNs << 1, DEFAULT_MAX_PARK_PERIOD_NS);
 	}
 
 	/**
 	 * Reset idling state.
 	 */
-	public void reset() {
-		yields = 0;
+	void reset() {
 		parkPeriodNs = DEFAULT_MIN_PARK_PERIOD_NS;
-		state = NOT_IDLE;
 	}
 
 }
